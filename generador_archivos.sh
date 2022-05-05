667f939b3da52f67f727ac86dce5da0a3c988a91
emtyloc <mitoro1@miuandes.cl>
2022-05-05 02:19:38 -0400

    GeneradorArchivos(read_input)Ready

--- generador_archivos.sh
+++ generador_archivos.sh
@@ -16,7 +16,20 @@ touch "0${i}_TORO_MATIAS.py"
 echo "import sys
 from collections import defaultdict
 
+def read_input(stdin:sys.stdin):
+    NDATAS = int(stdin.readline())
+    cases = []
+    for _ in range(NDATAS):
+        N,M = map(int, stdin.readline().split()) #Number of Junctions, Number of roads
+        case = [[N,M]]
+        for _ in range(M):
+            street = list(map(int, stdin.readline().split()))
+            case.append(street)
+        cases.append(case)
+    return cases
+
 def main():
+    cases = read_input(sys.stdin)
     print()
 
 if __name__ == '__main__':
